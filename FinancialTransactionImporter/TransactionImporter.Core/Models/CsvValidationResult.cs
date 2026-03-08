36c51df824824a94ddf605477892c808e1dfcc9c
Daniel Cornelius <danielwaynecornelius@gmail.com>
2026-03-08 11:49:17 +0200

    add entities and models

---
+++ FinancialTransactionImporter/TransactionImporter.Core/Models/CsvValidationResult.cs
@@ -0,0 +1,12 @@
+﻿
+using TransactionImporter.Core.Entities;
+
+namespace TransactionImporter.Core.Models
+{
+    public class CsvValidationResult
+    {
+        public bool IsValid { get; set; }
+        public List<ValidationError> Errors { get; set; } = [];
+        public List<Transaction> ParsedTransactions { get; set; } = [];
+    }
+}
---
+++ FinancialTransactionImporter/TransactionImporter.Core/Models/UpdateTransactionRequest.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TransactionImporter.Core.Models
+{
+    public class UpdateTransactionRequest
+    {
+        [Required]
+        public DateTime TransactionTime { get; set; }
+
+        [Required]
+        public decimal Amount { get; set; }
+
+        [Required]
+        [MinLength(1, ErrorMessage = "Description cannot be empty.")]
+        public string Description { get; set; } = string.Empty;
+    }
+}
